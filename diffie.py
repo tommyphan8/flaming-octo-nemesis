f8d09cb6685bb66be31ee5e65374612afa51e028
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-04-16 19:01:03 -0700

    initial commit

---
+++ diffie.py
@@ -0,0 +1,18 @@
+import random
+
+list = a()
+
+p = 19
+g = 3
+
+for x in range (1, p):
+	list.add(g**x % p)
+
+
+if ((len(list)==len(set(list)) == true)
+	x = random.choice(list)
+	y = random.choice(list)
+	x
+	y
+
+	
